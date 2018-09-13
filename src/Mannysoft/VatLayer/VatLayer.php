641e1297e913cd9ef34bed17e4a56d5d34e190b4
Manny Isles <manny.isles@gmail.com>
2018-09-13 16:27:03 +0800

    Update VatLayer.php

--- src/Mannysoft/VatLayer/VatLayer.php
+++ src/Mannysoft/VatLayer/VatLayer.php
@@ -24,13 +24,13 @@ class VatLayer {
         if ($vatNumber == '') {
             return false;
         }
-        // http://apilayer.net/api/validate?access_key=345d372cc739b313e7454aa5f1c6ae86&vat_number=CY10352115X
+        
         $response = $this->client->get(config('vatlayer.api_url') . 'validate?' . 
             'access_key=' . config('vatlayer.api_key') .
             '&vat_number=' . $vatNumber
         );
 
-        //$response = $this->client->get('https://vatlayer.com/php_helper_scripts/vat_api.php?secret_key=f891ab6f971af3c4b104ab8926979613&vat_number=' . $vatNumber);
+        
         if ($response->getStatusCode() == 200) {
             //$json = $response->json();
             $json = json_decode($response->getBody(), true);
